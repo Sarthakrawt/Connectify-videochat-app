81025911748211352b8b95304ac4b9f04a2e55b8
Sarthak Singh Rawat <sachinrumal@gmail.com>
2025-07-19 16:42:31 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 export const axiosInstance = axios.create({
-    baseURL: "https://connectify-videochat-app.onrender.com",
+    baseURL: "https://connectify-videochat-app.onrender.com/api",
     withCredentials: true,// send cookie
 })
