2aec58fc355220f4640f290907629ab637e07249
Sarthak Singh Rawat <sachinrumal@gmail.com>
2025-07-19 16:23:35 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://render.com/docs/web-services#port-bindingi",
     withCredentials: true,// send cookie
 })
