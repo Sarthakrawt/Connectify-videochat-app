1d03e48dc3a8cc8131aad50fd64a41a496fc8675
Sarthak Singh Rawat <sachinrumal@gmail.com>
2025-07-19 17:13:55 +0530

    Update auth.controller.js

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -48,7 +48,7 @@ try {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 *60 * 1000,
         httpOnly: true,
-        sameSite: none,
+        sameSite: "none",
          secure:true
     })
     res.status(201).json({success: true , user: newUser })
@@ -84,7 +84,7 @@ async function login(req,res ){
        res.cookie("jwt", token, {
         maxAge : 7 * 24 * 60 * 60 * 1000,
         httpOnly: true, 
-        sameSite: none ,
+        sameSite: "none" ,
         secure: true
        })
       return res.status(200).json({success: true, user})
